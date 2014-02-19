46928c98050f4784f1ceb9a4d6e4afca8473a758
Zhuang Ya <zhuangya@gmail.com>
2014-02-19 11:05:47 +0800

    introducing yeoman generated angular

---
+++ zxapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('zxappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
