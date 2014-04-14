e0ceeee72b18175ccc878f509f274450947cc009
lisztli <sockpuppet.lea@gmail.com>
2014-04-14 14:11:47 +0800

    merge from online

--- static/coffee/cover.coffee
+++ static/coffee/cover.coffee
@@ -2,22 +2,14 @@ $(document).ready ()->
     $('.cover-page .couple .c-left')
         .click () ->
             window.location = 'http://chi.zhangs.me/zc'
-        .mouseenter () ->
-            tgt = $('.cover-page .couple .c-right:first')
-            tgt.addClass 'c-unhover'
-            tgt.removeClass 'c-normal'
-        .mouseleave () ->
-            tgt = $('.cover-page .couple .c-right:first')
-            tgt.addClass 'c-normal'
-            tgt.removeClass 'c-unhover'
+        .mouseenter (event) ->
+            $(event.target).css 'margin-top', '-1px'
+        .mouseleave (event) ->
+            $(event.target).css 'margin-top', '0px'
     $('.cover-page .couple .c-right')
         .click () ->
             window.location = 'http://xuan.zhangs.me/zx'
-        .mouseenter () ->
-            tgt = $('.cover-page .couple .c-left:first')
-            tgt.addClass 'c-unhover'
-            tgt.removeClass 'c-normal'
-        .mouseleave () ->
-            tgt = $('.cover-page .couple .c-left:first')
-            tgt.addClass 'c-normal'
-            tgt.removeClass 'c-unhover'
+        .mouseenter (event) ->
+            $(event.target).css 'margin-top', '-1px'
+        .mouseleave (event) ->
+            $(event.target).css 'margin-top', '0px'
--- static/js/cover.js
+++ static/js/cover.js
@@ -3,28 +3,16 @@
 $(document).ready(function() {
   $('.cover-page .couple .c-left').click(function() {
     return window.location = 'http://chi.zhangs.me/zc';
-  }).mouseenter(function() {
-    var tgt;
-    tgt = $('.cover-page .couple .c-right:first');
-    tgt.addClass('c-unhover');
-    return tgt.removeClass('c-normal');
-  }).mouseleave(function() {
-    var tgt;
-    tgt = $('.cover-page .couple .c-right:first');
-    tgt.addClass('c-normal');
-    return tgt.removeClass('c-unhover');
+  }).mouseenter(function(event) {
+    return $(event.target).css('margin-top', '-1px');
+  }).mouseleave(function(event) {
+    return $(event.target).css('margin-top', '0px');
   });
   return $('.cover-page .couple .c-right').click(function() {
     return window.location = 'http://xuan.zhangs.me/zx';
-  }).mouseenter(function() {
-    var tgt;
-    tgt = $('.cover-page .couple .c-left:first');
-    tgt.addClass('c-unhover');
-    return tgt.removeClass('c-normal');
-  }).mouseleave(function() {
-    var tgt;
-    tgt = $('.cover-page .couple .c-left:first');
-    tgt.addClass('c-normal');
-    return tgt.removeClass('c-unhover');
+  }).mouseenter(function(event) {
+    return $(event.target).css('margin-top', '-1px');
+  }).mouseleave(function(event) {
+    return $(event.target).css('margin-top', '0px');
   });
 });
